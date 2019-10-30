9549a6949d333bbd450c72a6b1bacd15f2c634c9
Noman Kabeer <nomankabeerr@gmail.com>
2019-10-30 10:31:28 +0500

    Print the longest word in from string

---
+++ practice.py
@@ -0,0 +1,21 @@
+'''
+ * User: Noman Kabeer
+ * Date: 30-Oct-2019
+ * Time: 1:12 AM
+ * Print the longest word in from string
+'''
+
+def LongestWord(sen):
+    longestword = None
+    longestwordcount = 0
+    words = sen.split(' ')
+    totalwords = len(words)
+    for i in range(totalwords):
+        if (longestwordcount < len(words[i])):
+            longestwordcount = len(words[i])
+            longestword = words[i]
+    return longestword
+
+
+print(LongestWord('I am a php programmer'))
+print(LongestWord('longest word will print in this string'))
