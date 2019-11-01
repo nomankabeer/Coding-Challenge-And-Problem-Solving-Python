7e9494f367dd0b206ef985564006fbb8b8d9e8c8
Noman Kabeer <nomankabeerr@gmail.com>
2019-11-02 00:15:28 +0500

    FindTargetNumberByAddingTwoNumbersFromGivenArray

--- Basic/FindTargetNumberByAddingTwoNumbersFromGivenArray.py
+++ Basic/FindTargetNumberByAddingTwoNumbersFromGivenArray.py
@@ -2,7 +2,7 @@
  * User: Noman Kabeer
  * Date: 1-nov-2019
  * Time: 1:12 AM
- * Probles:
+ * Problem:
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  * Example:
