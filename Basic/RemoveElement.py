6eea41e8d6d61c1e0b67a4d043092cdc17ab037b
Noman Kabeer <nomankabeerr@gmail.com>
2019-11-04 23:47:41 +0500

    Remove Element

--- Basic/RemoveElement.py
+++ Basic/RemoveElement.py
@@ -1,4 +1,12 @@
-
+'''
+ * User: Noman Kabeer
+ * Date: 04-nov-2019
+ * Time: 1:12 AM
+ * Problem:
+ * Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ *
+'''
 def removeElement(nums, val):
     for key, num in enumerate(nums):
         print(num)
